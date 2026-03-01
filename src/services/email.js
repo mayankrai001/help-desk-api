55cc97c2d76813260a70420e2230370f2e249f0d
mayankrai001 <100416993+mayankrai001@users.noreply.github.com>
2026-03-02 00:46:32 +0530

    minor fix for emails

--- src/services/email.js
+++ src/services/email.js
@@ -4,10 +4,13 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for 465
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
