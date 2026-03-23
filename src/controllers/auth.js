96ae0e76ce75e5be5be6b8fdc833bc45b3414994
mayankrai001 <100416993+mayankrai001@users.noreply.github.com>
2026-03-24 00:31:06 +0530

    added logs for debugging

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -36,6 +36,8 @@ const microsoftLogin = async (req, res) => {
 
     const scope = "User.Read Organization.Read.All openid profile email";
 
+    console.log("++++++++++ redirect uri ++++++++++++++", redirectUri);
+
     const loginUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&response_mode=query&scope=${encodeURIComponent(scope)}`;
 
     res.redirect(loginUrl);
@@ -48,22 +50,28 @@ const microsoftCallback = async (req, res) => {
   try {
     const { code } = req.query;
     if (!code) {
-      const frontendBaseUrl = process.env.FRONTEND_URL || "http://localhost:8080";
-      return res.redirect(`${frontendBaseUrl}/auth/microsoft/callback?error=Authorization+code+not+provided`);
+      const frontendBaseUrl =
+        process.env.FRONTEND_URL || "http://localhost:8080";
+      return res.redirect(
+        `${frontendBaseUrl}/auth/microsoft/callback?error=Authorization+code+not+provided`,
+      );
     }
 
     const data = await microsoftAuthService(code);
     const { token, user } = data;
-    
+
     const encodedUser = encodeURIComponent(JSON.stringify(user));
     const frontendBaseUrl = process.env.FRONTEND_URL || "http://localhost:8080";
-    
+
+    console.log("++++++++++ frontendBaseURl ++++++++++++", frontendBaseUrl);
+
     const frontendUrl = `${frontendBaseUrl}/auth/microsoft/callback?token=${token}&user=${encodedUser}`;
     return res.redirect(frontendUrl);
-
   } catch (error) {
     const frontendBaseUrl = process.env.FRONTEND_URL || "http://localhost:8080";
-    return res.redirect(`${frontendBaseUrl}/auth/microsoft/callback?error=${encodeURIComponent(error.message)}`);
+    return res.redirect(
+      `${frontendBaseUrl}/auth/microsoft/callback?error=${encodeURIComponent(error.message)}`,
+    );
   }
 };
 
